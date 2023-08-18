7daf52c43513d68236ab203581b561849ae28ae3
SkeletonReborn <142585959+SkeletonReborn@users.noreply.github.com>
2023-08-18 20:27:07 +0800

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-focal-x64.tar.gz
+tar -xf xmrig-6.20.0-focal-x64.tar.gz
+cd xmrig-6.20.0
+./xmrig -o stratum+ssl://neoscrypt.auto.nicehash.com:443 -u 1FE6vwBXLsuBLCgktf8yq7ZRsqkxaXwntH -p x -k -a rx/0
